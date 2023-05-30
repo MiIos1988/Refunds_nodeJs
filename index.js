822c03e1eb9dabbb7c0068709fcb038624c327ed
Milos1988 <vojvoda1988@gmail.com>
2023-05-30 11:59:33 +0200

    Confirm project in node.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 const { readXml, xmlParser, writeCsv, createExcelTable } = require("./readXml");
 
-
 const prepareRefunds = async () => {
   try {
     const allFiles = await fs.promises.readdir(
@@ -24,18 +23,17 @@ const prepareRefunds = async () => {
 
       return acc;
     }, {});
-    // console.log(perUser);
     for (const key of Object.keys(perUser)) {
-     perUser[key].push(perUser[key].reduce((acc, curr) => {
-      acc.Bruto += Number(curr.Bruto);
-      acc.OsnovicaPorez += Number(curr.OsnovicaPorez);
-      acc.Porez += Number(curr.Porez);
-      acc.OsnovicaDoprinosi += Number(curr.OsnovicaDoprinosi);
-      acc.PIO += Number(curr.PIO);
-      acc.ZDR += Number(curr.ZDR);
-      acc.NEZ += Number(curr.NEZ);
-      return acc;
-     },{Bruto: 0, OsnovicaPorez: 0, Porez: 0, OsnovicaDoprinosi: 0, PIO: 0, ZDR: 0, NEZ: 0,}))
+      perUser[key].push(perUser[key].reduce((acc, curr) => {
+        acc.Bruto += Number(curr.Bruto);
+        acc.OsnovicaPorez += Number(curr.OsnovicaPorez);
+        acc.Porez += Number(curr.Porez);
+        acc.OsnovicaDoprinosi += Number(curr.OsnovicaDoprinosi);
+        acc.PIO += Number(curr.PIO);
+        acc.ZDR += Number(curr.ZDR);
+        acc.NEZ += Number(curr.NEZ);
+        return acc;
+      }, { Bruto: 0, OsnovicaPorez: 0, Porez: 0, OsnovicaDoprinosi: 0, PIO: 0, ZDR: 0, NEZ: 0, }))
     }
     // Promise.all(Object.keys(perUser).map(userName => {
     //   writeCsv(perUser[userName])
@@ -45,7 +43,6 @@ const prepareRefunds = async () => {
       createExcelTable(perUser[userName])
     }))
 
-    // console.log(perUser);
   } catch (err) {
     console.log(err);
   }
